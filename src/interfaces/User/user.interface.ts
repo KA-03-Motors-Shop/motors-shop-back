b0a452bb6fcf6ade6d8ca7c80b2e4f6762d81a9b
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-08-16 19:24:15 -0300

    feat:remove email from interface dataToken

--- src/interfaces/User/user.interface.ts
+++ src/interfaces/User/user.interface.ts
@@ -5,7 +5,6 @@ export type UserCreation = Omit<User, 'id' | 'createdAt' | 'vehicle'>;
 export type userLogin = Pick<User, 'email' | 'password'>;
 
 export interface dataToken {
-	email: string;
 	id: string;
 	iat: number;
 	exp: number;

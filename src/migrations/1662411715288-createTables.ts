64377b3c59b05f941beca242a27e41d500b8eeb6
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-09-05 18:07:43 -0300

    createTables

--- src/migrations/1662411715288-createTables.ts
+++ src/migrations/1662411715288-createTables.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class createNewTables1662411126148 implements MigrationInterface {
-    name = 'createNewTables1662411126148'
+export class createTables1662411715288 implements MigrationInterface {
+    name = 'createTables1662411715288'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "vehicle" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "advertisement_type" character varying(128) NOT NULL, "title" character varying(128) NOT NULL, "fabrication_year" character varying(4) NOT NULL, "mileage" character varying(64) NOT NULL, "price" character varying(32) NOT NULL, "description" text NOT NULL, "vehicle_type" character varying(128) NOT NULL, "is_active" boolean NOT NULL, "userId" uuid, CONSTRAINT "PK_187fa17ba39d367e5604b3d1ec9" PRIMARY KEY ("id"))`);

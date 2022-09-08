e177e6d2ca5995da300667876c1e7d2448d5cbee
Rafael Ricciardi <ricciardi.rafael1997@gmail.com>
2022-09-08 10:23:19 -0300

    feat: refact delete service vehicle

--- src/services/adverts/deleteAdverts.service.ts
+++ src/services/adverts/deleteAdverts.service.ts
@@ -1,27 +1,30 @@
-import { DeleteResult } from "typeorm";
-import { AppDataSource } from "../../data-source";
-import { AppError } from "../../errors";
+import { DeleteResult } from 'typeorm';
+import { AppDataSource } from '../../data-source';
+import { AppError } from '../../errors';
+import { Vehicle } from '../../entities/Vehicles';
 
 interface ProductDataParams {
-    id: string;
+	id: string;
 }
 
 class DeleteAdvertsService {
-    async execute({ id }: ProductDataParams): Promise<DeleteResult> {
-        const productRepository = AppDataSource.getRepository(id);
+	async execute({ id }: ProductDataParams): Promise<DeleteResult> {
+		const productRepository = AppDataSource.getRepository(Vehicle);
 
-        const adverts = await productRepository.findOne({
-            where: {
-                id,
-            },
-        });
+		// const adverts = await productRepository.findOne({
+		// 	where: {
+		// 		id,
+		// 	},
+		// });
 
-        if (!adverts) {
-            throw new AppError(404, "Not found any product with this id");
-        }
+		const adverts = await productRepository.findOneBy({ id: id });
 
-        return await productRepository.delete(id);
-    }
+		if (!adverts) {
+			throw new AppError(404, 'Not found any product with this id');
+		}
+
+		return await productRepository.delete(adverts);
+	}
 }
 
 export default DeleteAdvertsService;
